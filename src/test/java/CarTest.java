abe4710a72f01e177843d1b29f83f4062c2dad1f
zinzoddari <zinzoddari@gmail.com>
2022-12-21 00:43:26 +0900

    test: 자동차 입력 관련 오류 메서드 코드 작성

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,10 +1,21 @@
 import domain.Car;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
 public class CarTest {
     @Test
     void 자동차_하나_입력_성공() {
         String input = "zinzo";
         Car car = new Car(input);
+    }
+
+    @Test
+    void 자동차_하나_입력_오류() {
+        String input = "abcdef";
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            Car car = new Car(input);
+        });
     }
 }
