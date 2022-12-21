3536c4a6bbc7ee3e7ed683201bbc5e523b3eb3ff
zinzoddari <zinzoddari@gmail.com>
2022-12-22 00:03:42 +0900

    fix: Cars 초기화 관련 추가

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -8,10 +8,24 @@ import java.util.List;
 public class Cars {
 
     public static final String CAR_NAME_DELIM = ",";
+    public static final int CARS_MIN_SIZE = 1;
+    public static final int CARS_MAX_SIZE = 9;
     private final List<Car> car;
+    private final int loop;
 
     public Cars(String input) {
-         this.car = setCar(input);
+        this.car = validateCars(setCar(input));
+        this.loop = 0;
+    }
+
+    public Cars(String input, int count) {
+         this.car = validateCars(setCar(input));
+         this.loop = count;
+    }
+
+    private List<Car> validateCars(List<Car> carList) {
+        if(CARS_MIN_SIZE <= carList.size() && CARS_MAX_SIZE >= carList.size()) return carList;
+        throw new IllegalArgumentException("[ERROR] 한자릿 수까지만 가능합니다.");
     }
 
     private List<Car> setCar(String input) {
@@ -23,5 +37,9 @@ public class Cars {
         }
 
         return carList;
+    }
+
+    public int getSize() {
+        return car.size();
     }
 }
