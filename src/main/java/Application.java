e49fabf56da4f5ee311c667a269ecf8306f2a0c5
zinzoddari <zinzoddari@gmail.com>
2022-12-29 01:51:15 +0900

    feat: main 메소드 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,10 @@
 import controller.RacingController;
+import service.RacingService;
 
 public class Application {
 
     public static void main(String[] args) {
-        RacingController racingController = new RacingController();
+        RacingController racingController = new RacingController(new RacingService());
         racingController.start();
     }
 }
