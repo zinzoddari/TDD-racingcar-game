957a7598e288777ecf41e907ddc1da9ca5d8fcc0
zinzoddari <zinzoddari@gmail.com>
2022-12-30 15:08:58 +0900

    feat: 반복횟수 입력받는 메소드 생성

--- src/main/java/service/RacingService.java
+++ src/main/java/service/RacingService.java
@@ -2,6 +2,7 @@ package service;
 
 import domain.Cars;
 import utils.Console;
+import view.Print;
 import view.PrintIn;
 
 public class RacingService {
@@ -24,5 +25,24 @@ public class RacingService {
         }
 
         return cars;
+    }
+
+    public int carPlay() {
+        int loop = 0;
+
+        PrintIn.printInputLoop();
+
+        while(true) {
+            try {
+                loop = Integer.parseInt(Console.readLine());
+            }
+            catch (IllegalArgumentException e) {
+                continue;
+            }
+
+            break;
+        }
+
+        return loop;
     }
 }
